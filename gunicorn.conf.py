5463f2c3b17ff17f751c49b0100ec1b0ac3a46ac
Evgeny Kryukov <krukov@bpcbt.com>
2017-07-17 15:40:32 +0300

    add gunicorn config

---
+++ gunicorn.conf.py
@@ -0,0 +1,14 @@
+import multiprocessing
+
+# bind = "127.0.0.1:8888"
+
+bind = "unix:///srv/publish-app/publish-app.sock"
+# worker numbers 2xCPUs + 1
+workers = multiprocessing.cpu_count() * 2 + 1
+user = "www-data"
+group = "www-data"
+errorlog = "/srv/publish-app/logs/error-gunicorn.log"
+loglevel = "info"
+proc_name = "publish-app"
+accesslog = "/srv/publish-app/logs/access-gunicorn.log"
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'

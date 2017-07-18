1d3ad8cc453af3819e586e030cb34740710eb8f8
Evgeny Kryukov <krukov@bpcbt.com>
2017-07-18 19:15:43 +0300

    Bump version

--- main.py
+++ main.py
@@ -27,15 +27,26 @@ def hello_world():
 
 
 def mk_dir(project, product, version, build_type, module_name, custom):
+
     path = '{base}{sep}{project}{sep}{product}{sep}'
+
     if build_type != 'stable':
         pattern = '{type}-builds{sep}'.format(type=build_type, sep=os.sep)
         path += pattern
-    path += '{version}{sep}{module}{sep}'
+
+    path += '{version}{sep}'
+
+    if module_name:
+        path += module_name
+        path += os.sep
+
     if custom:
         path += custom
-    path = path.format(base=settings.SAVE_PATH, sep=os.sep, project=project, product=product, version=version,
-                       module=module_name)
+        path += os.sep
+
+    path = path.format(base=settings.SAVE_PATH, sep=os.sep, project=project, product=product, version=version)
+
     if not os.path.isdir(path):
         os.makedirs(path)
+
     return path
